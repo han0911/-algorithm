7b5f9f99349a21480757c2226c7dfe9f116972b6
han0911 <a01099249559@gmail.com>
2026-08-12 09:42:19 +0900

    프로그래머스 주사위 게임 2

---  Condition/ condition8.java
+++  Condition/ condition8.java
@@ -1,9 +1,18 @@
 class Solution {
-    public int solution(int a, int b, boolean flag) {
-        if(flag){
-            return a+b;
-        }else{
-            return a-b;
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+
+        if (a == b && b == c) {
+            answer = (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
         }
+        
+        else if (a == b || b == c || a == c) {
+            answer = (a + b + c) * (a*a + b*b + c*c);
+        }
+        else {
+            answer = a + b + c;
+        }
+
+        return answer;
     }
 }
