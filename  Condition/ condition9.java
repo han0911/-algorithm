9c9e6ab203b981d1f279a8d1e7f99285a7f62fa7
han0911 <a01099249559@gmail.com>
2026-08-12 09:42:47 +0900

    프로그래머스 원소들의 곱과 합

---  Condition/ condition9.java
+++  Condition/ condition9.java
@@ -1,9 +1,16 @@
 class Solution {
-    public int solution(int a, int b, boolean flag) {
-        if(flag){
-            return a+b;
+    public int solution(int[] num_list) {
+        int answer1 = 0;
+        int answer2 = 1;
+        
+        for(int i = 0;i<=num_list.length-1;i++){
+            answer1 += num_list[i];
+            answer2 *= num_list[i];
+        }
+        if((answer1*answer1) > answer2){
+            return 1;
         }else{
-            return a-b;
+            return 0;
         }
     }
 }
