b2cf8bfc831d73cfee043d9e91edded5e8bc2f9d
han0911 <a01099249559@gmail.com>
2026-08-12 09:48:43 +0900

    프로그래머스 카운트 업

--- For/For8.java
+++ For/For8.java
@@ -1,23 +1,16 @@
+import java.util.ArrayList;
+
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
-
-        for (int i = 0; i < queries.length; i++) {
-            int s = queries[i][0];
-            int e = queries[i][1];
-            int k = queries[i][2];
-            
-            int min = -1;
-
-            for (int j = s; j <= e; j++) {
-                if (arr[j] > k) {
-                    if (min == -1 || arr[j] < min) {
-                        min = arr[j];
-                    }
-                }
-            }
-            answer[i] = min;
+    public int[] solution(int start_num, int end_num) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = start_num; i <= end_num; i++) {
+            list.add(i);
         }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
         return answer;
     }
 }
