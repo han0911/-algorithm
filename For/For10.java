f83d51e1204ec50ba28e79a8b091450bdb0dcd48
han0911 <a01099249559@gmail.com>
2026-08-12 09:49:32 +0900

    프로그래머스 배열 만들기 4

--- For/For10.java
+++ For/For10.java
@@ -1,23 +1,27 @@
+import java.util.ArrayList;
+
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = new int[queries.length];
-
-        for (int i = 0; i < queries.length; i++) {
-            int s = queries[i][0];
-            int e = queries[i][1];
-            int k = queries[i][2];
-            
-            int min = -1;
-
-            for (int j = s; j <= e; j++) {
-                if (arr[j] > k) {
-                    if (min == -1 || arr[j] < min) {
-                        min = arr[j];
-                    }
-                }
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        int i = 0;
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+            } else {
+                stk.remove(stk.size() - 1);
             }
-            answer[i] = min;
         }
+
+        int[] answer = new int[stk.size()];
+
+        for (int j = 0; j < stk.size(); j++) {
+            answer[j] = stk.get(j);
+        }
+
         return answer;
     }
 }
