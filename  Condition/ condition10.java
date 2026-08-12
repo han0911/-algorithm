03844b6c45d20eb3b1003da5e578ae7b27aedd10
han0911 <a01099249559@gmail.com>
2026-08-12 09:43:16 +0900

    프로그래머스 이어 붙인 수

---  Condition/ condition10.java
+++  Condition/ condition10.java
@@ -1,9 +1,14 @@
 class Solution {
-    public int solution(int a, int b, boolean flag) {
-        if(flag){
-            return a+b;
-        }else{
-            return a-b;
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+        for(int i = 0;i<=num_list.length-1;i++){
+            if(num_list[i] % 2==0){
+                even += num_list[i];
+            }else if (num_list[i] % 2==1){
+                odd += num_list[i];
+            }
         }
+        return Integer.parseInt(odd) + Integer.parseInt(even);
     }
 }
