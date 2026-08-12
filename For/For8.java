65233c39ad446f4c96c866e5cceb3d6c69af80b8
han0911 <a01099249559@gmail.com>
2026-08-12 09:47:50 +0900

    프로그래머스 수열과 구간 쿼리 4

---
+++ For/For8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            
+            int min = -1;
+
+            for (int j = s; j <= e; j++) {
+                if (arr[j] > k) {
+                    if (min == -1 || arr[j] < min) {
+                        min = arr[j];
+                    }
+                }
+            }
+            answer[i] = min;
+        }
+        return answer;
+    }
+}
