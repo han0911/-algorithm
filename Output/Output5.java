8db9b296ce1cdfc664a5f25818de40a7a5ff183d
han0911 <a01099249559@gmail.com>
2026-08-12 09:29:00 +0900

    프로그래머스 문자열 겹쳐쓰기

---
+++ Output/Output5.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        int overwriteLen = overwrite_string.length();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (i >= s && i < s + overwriteLen) {
+                answer += overwrite_string.charAt(i - s);
+            } else {
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
